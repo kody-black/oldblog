9a900f0bba4647cf56cd303ccbf7f514b79df16b
distiny-cool <2442658787@qq.com>
2022-11-19 17:43:30 +0800

    New-Blog ps1

---
+++ _posts/New-Blog.ps1
@@ -0,0 +1,29 @@
+﻿param(
+    $title='title',
+    $sub='',
+    $tag=''
+)
+
+$a = 
+"---
+layout:     post
+title:      "+$title+"
+subtitle:   "+$sub+"
+date:       "
+
+$time = Get-Date -UFormat "%Y-%m-%d"
+
+$b = 
+"
+author:     Kody Black
+header-img: img/post-bg-normal.jpg
+catalog: true
+tags:
+    - "+$tag+"
+---
+
+"
+
+$content = $a + $time + $b
+
+$content | Out-File $time-$title".md"  -NoClobber
